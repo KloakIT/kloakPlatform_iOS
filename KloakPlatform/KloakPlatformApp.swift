ee9e23b95105ffc9e78f893cd0394b5bc13a804f
Kloak Information Technologies Inc <info@kloak.app>
2020-11-18 20:46:24 -0800

    Initial Commit

---
+++ KloakPlatform/KloakPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  KloakPlatformApp.swift
+//  KloakPlatform
+//
+//  Created by Peter Xie on 2020-11-18.
+//
+
+import SwiftUI
+
+@main
+struct KloakPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
